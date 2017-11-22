3e97926147a1b3c2b0d7c21e7cb98567f3cc5e83
MezentsevIlya <ilyamezentcev@gmail.com>
2017-11-22 10:34:27 +0500

    Add files via upload

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -417,6 +417,31 @@ namespace Solution
 			}
 			return -1;
 		}
+		public static int[] PlusOne(int[] digits) {
+			int n = digits.Length;
+			for(int i = n - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+			}
+		
+		int[] newNumber = new int [n+1];
+		newNumber[0] = 1;
+		
+		return newNumber;
+		}
+		
+		public static int MySqrt(int x) {
+			if (x == 0 || x == 1)
+				return x;
+			for (int i = 0; i <= x / 2; i++)
+				if (i * i <= x && ((i + 1) * (i + 1) > x || (i + 1) * (i + 1) < 0))
+					return i;
+			return 0;
+		}
+		
         static void Main(string[] args)
         {
             IList<string> a = LetterCombinations("23");
